699b0be4f062619417095acf3b645efeb7eab96f
sk <sor753@gmail.com>
2026-06-30 17:43:08 +0900

    locator を追加

--- e2e/UIBasicstest.spec.ts
+++ e2e/UIBasicstest.spec.ts
@@ -5,6 +5,25 @@ test("ブラウザコンテキストのテスト", async ({browser}) => {
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
+
+    const userName = page.locator("#username");
+    const signInBtn = page.locator("#signInBtn");
+    const cardTitles = page.locator(".card-body a");
+
+    await userName.fill("rahulshetty");
+    await page.locator("[type='password']").fill("Learning@830$3mK2");
+    await signInBtn.click();
+    console.log(await page.locator("[style*='block']").textContent())
+    await expect( page.locator("[style*='block']")).toContainText("Incorrect");
+
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy");
+    await signInBtn.click();
+    // console.log(await cardTitles.first().textContent());
+    // console.log(await cardTitles.nth(0).textContent());
+
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
 })
 
 test("ページのテスト", async ({page}) => {
